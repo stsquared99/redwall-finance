59931bbbee5cc3dac7b3a51d4151afc67123593b
Samuel Tran <samuel.tran@liferay.com>
2019-05-18 19:53:44 -0700

    Implement basic /user POST

---
+++ redwall-finance-api/api/controllers/user.js
@@ -0,0 +1,27 @@
+'use strict';
+var util = require('util');
+var models = require('../../models');
+
+var sequelize = models.sequelize
+var User = models.User;
+
+module.exports = {add};
+
+//POST /user
+function add(req, res) {
+  sequelize.sync().then(
+    () => User.create(req.swagger.params.user.value)
+  ).then(
+    user => res.json(user.toJSON())
+  ).catch(function(err) {
+    res.status(400);
+
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.json({"message":"Duplicate email"});
+    } else {
+      res.json({"message":err.name});
+
+      console.error(err);
+    }
+  });
+}
--- redwall-finance-api/app.js
+++ redwall-finance-api/app.js
@@ -2,22 +2,25 @@
 
 var SwaggerConnect = require('swagger-connect');
 var app = require('connect')();
+var models = require('./models');
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerConnect.create(config, function(err, swaggerConnect) {
-  if (err) { throw err; }
+models.sequelize.sync().then(function() {
+  SwaggerConnect.create(config, function(err, swaggerConnect) {
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerConnect.register(app);
+    // install middleware
+    swaggerConnect.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+    var port = process.env.PORT || 10010;
+    app.listen(port);
 
-  if (swaggerConnect.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    if (swaggerConnect.runner.swagger.paths['/hello']) {
+      console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
+  });
 });

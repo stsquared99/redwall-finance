41aae0be27e3b0a7f7ae89bbc5bcf64ad240ba79
Samuel Tran <samuel.tran@liferay.com>
2019-05-20 19:00:37 -0700

    basic transactions

--- redwall-finance-api/api/controllers/account.js
+++ redwall-finance-api/api/controllers/account.js
@@ -5,11 +5,135 @@ var models = require('../../models');
 
 var sequelize = models.sequelize;
 var Account = models.Account;
+var Transaction = models.Transaction;
 
 module.exports = {
+  doATMTransaction,
+  doDebitTransaction,
   getAccount,
   updateAccount
 };
+
+//POST /account/{accountNumber}/atm
+function doATMTransaction(req, res) {
+  sequelize.transaction(t => {
+    return Account.findOne({
+      where: {
+        userId: req.swagger.params.accountNumber.value
+      }
+    }).then(account => {
+      if (account === null) {
+        throw new Error('Account not found');
+      }
+
+      var accountJson = account.toJSON();
+
+      var amountInCents = req.swagger.params.transactionProperties.value.amountInCents;
+      var type = req.swagger.params.transactionProperties.value.type;
+
+      var createTransactionProperties;
+
+      if (type === 'DEPOSIT') {
+        createTransactionProperties = {
+          amountInCents: amountInCents,
+          description: 'ATM ' + type,
+          fromAccountType: 'ATM',
+          fromAccountNumber: 0,
+          fromRoutingNumber: 0,
+          toAccountType: 'INTERNAL',
+          toAccountNumber: accountJson.accountNumber,
+          toRoutingNumber: accountJson.routingNumber
+        };
+      } else {
+        createTransactionProperties = {
+          amountInCents: amountInCents,
+          description: 'ATM ' + type,
+          fromAccountType: 'INTERNAL',
+          fromAccountNumber: accountJson.accountNumber,
+          fromRoutingNumber: accountJson.routingNumber,
+          toAccountType: 'ATM',
+          toAccountNumber: 0,
+          toRoutingNumber: 0
+        };
+
+      }
+
+      return Transaction.create(createTransactionProperties);
+    });
+  }).then(
+    transaction => {
+      res.json(transaction.toJSON());
+    }
+  ).catch(function(err) {
+    res.status(400);
+
+    res.json({
+      'message': err.message
+    });
+
+    console.error(err);
+  });
+}
+
+//POST /account/{accountNumber}/debit
+function doDebitTransaction(req, res) {
+  sequelize.transaction(t => {
+    return Account.findOne({
+      where: {
+        userId: req.swagger.params.accountNumber.value
+      }
+    }).then(account => {
+      if (account === null) {
+        throw new Error('Account not found');
+      }
+
+      var accountJson = account.toJSON();
+
+      var amountInCents = req.swagger.params.transactionProperties.value.amountInCents;
+      var type = req.swagger.params.transactionProperties.value.type;
+
+      var createTransactionProperties;
+
+      if (type === 'CHARGE') {
+        createTransactionProperties = {
+          amountInCents: amountInCents,
+          description: 'DEBIT ' + type,
+          fromAccountType: 'INTERNAL',
+          fromAccountNumber: accountJson.accountNumber,
+          fromRoutingNumber: accountJson.routingNumber,
+          toAccountType: 'DEBIT',
+          toAccountNumber: 0,
+          toRoutingNumber: 0
+        };
+      } else {
+        createTransactionProperties = {
+          amountInCents: amountInCents,
+          description: 'DEBIT ' + type,
+          fromAccountType: 'DEBIT',
+          fromAccountNumber: 0,
+          fromRoutingNumber: 0,
+          toAccountType: 'INTERNAL',
+          toAccountNumber: accountJson.accountNumber,
+          toRoutingNumber: accountJson.routingNumber
+        };
+      }
+
+      return Transaction.create(createTransactionProperties);
+    });
+  }).then(
+    transaction => {
+      res.json(transaction.toJSON());
+    }
+  ).catch(function(err) {
+    res.status(400);
+
+    res.json({
+      'message': err.message
+    });
+
+    console.error(err);
+  });
+}
 
 //GET /account/{id} operationId
 function getAccount(req, res, next) {

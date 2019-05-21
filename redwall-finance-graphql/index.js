1f0d6f07d57c3948876e4b3ddee06cab3d81bb15
Samuel Tran <samuel.tran@liferay.com>
2019-05-20 23:08:22 -0700

    basic graphql server

---
+++ redwall-finance-graphql/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphQLSchema = require('swagger-to-graphql');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+
+const proxyUrl = 'http://localhost:10010';
+const pathToSwaggerSchema = './redwall.json';
+const customHeaders = {}
+
+// Initialize the app
+const app = express();
+
+graphQLSchema(pathToSwaggerSchema, proxyUrl, customHeaders).then(schema => {
+  app.use('/graphql', graphqlHTTP(() => {
+    return {
+      schema,
+      graphiql: true
+    };
+  }));
+
+  // GraphiQL, a visual editor for queries
+  app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+  app.listen(3009, () => {
+    console.log('Go to http://localhost:3009/graphiql to run queries!');
+  });
+}).catch(e => {
+  console.log(e);
+});

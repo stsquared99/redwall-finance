f2a2dd7864195ac0dee6225b02356187e231759d
Samuel Tran <samuel.tran@liferay.com>
2019-05-21 09:07:03 -0700

    generate tests

---
+++ redwall-finance-api/test/api/client/user-{userId}-test.js
@@ -0,0 +1,173 @@
+'use strict';
+var chai = require('chai');
+var sequelizeFixtures = require('sequelize-fixtures');
+var ZSchema = require('z-schema');
+var customFormats = module.exports = function(zSchema) {
+  // Placeholder file for all custom-formats in known to swagger.json
+  // as found on
+  // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#dataTypeFormat
+
+  var decimalPattern = /^\d{0,8}.?\d{0,4}[0]+$/;
+
+  /** Validates floating point as decimal / money (i.e: 12345678.123400..) */
+  zSchema.registerFormat('double', function(val) {
+    return !decimalPattern.test(val.toString());
+  });
+
+  /** Validates value is a 32bit integer */
+  zSchema.registerFormat('int32', function(val) {
+    // the 32bit shift (>>) truncates any bits beyond max of 32
+    return Number.isInteger(val) && ((val >> 0) === val);
+  });
+
+  zSchema.registerFormat('int64', function(val) {
+    return Number.isInteger(val);
+  });
+
+  zSchema.registerFormat('float', function(val) {
+    // better parsing for custom "float" format
+    if (Number.parseFloat(val)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  zSchema.registerFormat('date', function(val) {
+    // should parse a a date
+    return !isNaN(Date.parse(val));
+  });
+
+  zSchema.registerFormat('dateTime', function(val) {
+    return !isNaN(Date.parse(val));
+  });
+
+  zSchema.registerFormat('password', function(val) {
+    // should parse as a string
+    return typeof val === 'string';
+  });
+};
+
+customFormats(ZSchema);
+
+var validator = new ZSchema({});
+var supertest = require('supertest');
+var api = supertest('http://localhost:10010'); // supertest init;
+var expect = chai.expect;
+
+describe('/user/{userId}', function() {
+  describe('delete', function() {
+    it('should respond with 204 No Content', function(done) {
+      api.del('/user/{userId PARAM GOES HERE}')
+      .set('Content-Type', 'application/json')
+      .expect(204)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(res.body).to.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+    
+  });
+
+  describe('get', function() {
+    it('should respond with 200 Success', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "userId",
+          "firstName",
+          "lastName",
+          "email",
+          "createdAt",
+          "updatedAt"
+        ],
+        "properties": {
+          "userId": {
+            "type": "integer"
+          },
+          "firstName": {
+            "type": "string"
+          },
+          "lastName": {
+            "type": "string"
+          },
+          "email": {
+            "type": "string"
+          },
+          "createdAt": {
+            "type": "string"
+          },
+          "updatedAt": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.get('/user/{userId PARAM GOES HERE}')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+  });
+
+  describe('patch', function() {
+    it('should respond with 200 Success', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "required": [
+          "userId",
+          "firstName",
+          "lastName",
+          "email",
+          "createdAt",
+          "updatedAt"
+        ],
+        "properties": {
+          "userId": {
+            "type": "integer"
+          },
+          "firstName": {
+            "type": "string"
+          },
+          "lastName": {
+            "type": "string"
+          },
+          "email": {
+            "type": "string"
+          },
+          "createdAt": {
+            "type": "string"
+          },
+          "updatedAt": {
+            "type": "string"
+          }
+        }
+      };
+
+      /*eslint-enable*/
+      api.patch('/user/{userId PARAM GOES HERE}')
+      .set('Content-Type', 'application/json')
+      .send({
+        userProperties: 'DATA GOES HERE'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        expect(validator.validate(res.body, schema)).to.be.true;
+        done();
+      });
+    });
+
+  });
+
+});
